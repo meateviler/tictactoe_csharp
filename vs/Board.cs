3596f0784c5db3f070daba2eabd876af5566ad41
meateviler <meateviler@msn.com>
2016-08-19 13:36:40 +0800

    add Board class

---
+++ vs/Board.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tictactoe_csharp
+{
+    public class Board
+    {
+        public const char EMPTY_CELL = '-';
+        public Board(string s)
+        {
+            data = new StringBuilder(s);
+        }
+
+        public Board()
+        {
+            data = new StringBuilder();
+        }
+
+        public StringBuilder data
+        {
+            get;
+            set;
+        }
+
+        public void SetPlayer(int position, char player)
+        {
+            data[position] = player;
+        }
+
+        public bool IsRowSamePlayer(int pos)
+        {
+            return data[pos] != EMPTY_CELL && data[pos] == data[pos+1] && data[pos+1] == data[pos+2];
+        }
+        public char GetPosPlayer(int pos)
+        {
+            return data[pos];
+        }
+        public bool IsEmpty(int pos)
+        {
+            return data[pos] == EMPTY_CELL;
+        }
+    }
+}
